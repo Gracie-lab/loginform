e59f25742a6a37b05d5e3ae95a93e7a3de18f0ff
Gracie-lab <gracebolutife@gmail.com>
2022-01-24 20:19:17 +0100

    first commit

---
+++ src/components/form/form.js
@@ -0,0 +1,25 @@
+function Form() {
+    return (
+        <>
+            {/*<nav className="navigation-menu--mobile-container" role="navigation">*/}
+            <div className="form">
+                <h4>Login</h4>
+                <form>
+                    <label>
+                        Email Address:
+                    </label>
+                    <br/> <br/>
+                    <input type="text" name="" />
+                    <br/> <br/>
+                    <input type="submit" value="Login" />
+                </form>
+
+
+                </div>
+
+            {/*</nav>*/}
+        </>
+    );
+}
+
+export default Form;
---
+++ src/components/header/Header.js
@@ -0,0 +1,32 @@
+import './headerstyle.css'
+import '../icons/Ellipse 6.png'
+import '../icons/Ellipse 7.png'
+import '../icons/Ellipse 8.png'
+
+function Header() {
+    return (
+        <>
+            {/*<nav className="navigation-menu--mobile-container" role="navigation">*/}
+            <div className="header-style">
+                    <div>
+                        <h1 className="text">Burger</h1>
+                    </div>
+                    <div className= "header-links">
+                        <h4 >Home</h4>
+                        <h4 >Services</h4>
+                        <h4 >About Us</h4>
+                        <h4 >Login</h4>
+                        <h4 >Sign Up</h4>
+                    </div>
+                <div>
+                    <img src="Ellipse 6" alt="facebook"/>
+                    <img src="Ellipse 7" alt="instagram"/>
+                    <img src="Ellipse 8" alt="twitter"/>
+                </div>
+            </div>
+            {/*</nav>*/}
+        </>
+    );
+}
+
+export default Header;
